a320d6363e27658063a9af029def7623b99d9835
Mritunjay <mritunjaygupta004@gmail.com>
2024-02-16 02:17:50 +0530

    still trying

--- next.config.js
+++ next.config.js
@@ -1,6 +1,35 @@
 // next.config.js
 
-module.exports = {
+// module.exports = {
+//   webpack(config) {
+//     config.module.rules.push({
+//       test: /\.svg$/i,
+//       issuer: /\.[jt]sx?$/,
+//       use: ["@svgr/webpack"],
+//     });
+
+//     return config;
+//   },
+//   reactStrictMode: true,
+//   eslint: {
+//     ignoreDuringBuilds: true,
+//   },
+//   env: {
+//     baseUrl: "https://api.brightdigigold.com",
+//     // baseUrl: "https://devapi.brightdigigold.com",
+//     // cashfree: "sandbox",
+//     cashfree: "production",
+//     // baseUrl: "http://localhost:3031",
+//     // baseUrl: "http://192.168.1.6:3031",
+//   },
+//   swcMinify: true,
+//   images: {
+//     unoptimized: true,
+//   },
+// };
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
@@ -14,6 +43,11 @@ module.exports = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  // basePath: '/beta',
+  // assetPrefix: '/beta',
+  // publicRuntimeConfig: {
+  //   baseURL: '/beta',
+  // },
   env: {
     baseUrl: "https://api.brightdigigold.com",
     // baseUrl: "https://devapi.brightdigigold.com",
@@ -22,8 +56,11 @@ module.exports = {
     // baseUrl: "http://localhost:3031",
     // baseUrl: "http://192.168.1.6:3031",
   },
+  reactStrictMode: true,
   swcMinify: true,
   images: {
     unoptimized: true,
   },
 };
+
+module.exports = nextConfig;
