c329101942cf4c41e80dc306f301c46851375a9b
Amit kumar Singh <notgetin18@gmail.com>
2024-02-16 03:07:38 +0530

    experiment

--- next.config.js
+++ next.config.js
@@ -1,33 +1,3 @@
-// next.config.js
-
-// module.exports = {
-//   webpack(config) {
-//     config.module.rules.push({
-//       test: /\.svg$/i,
-//       issuer: /\.[jt]sx?$/,
-//       use: ["@svgr/webpack"],
-//     });
-
-//     return config;
-//   },
-//   reactStrictMode: true,
-//   eslint: {
-//     ignoreDuringBuilds: true,
-//   },
-//   env: {
-//     baseUrl: "https://api.brightdigigold.com",
-//     // baseUrl: "https://devapi.brightdigigold.com",
-//     // cashfree: "sandbox",
-//     cashfree: "production",
-//     // baseUrl: "http://localhost:3031",
-//     // baseUrl: "http://192.168.1.6:3031",
-//   },
-//   swcMinify: true,
-//   images: {
-//     unoptimized: true,
-//   },
-// };
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack(config) {
