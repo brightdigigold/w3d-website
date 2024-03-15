a28d70dc9b4211fdb94bce9a5e833f55829a8a7f
Amit kumar <92637182+notgetin18@users.noreply.github.com>
2024-03-15 13:50:34 +0530

    Update mixPannelEvents.tsx

--- src/components/analytics/mixPannelEvents.tsx
+++ src/components/analytics/mixPannelEvents.tsx
@@ -1,6 +1,9 @@
 
 
 export const MixPannelEvents = (event_name: string, properties: {}) => {
+    console.log("event_name", event_name)
+    console.log("properties", properties)
+    
     try {
         if ((window as any).mixpanel) {
             (window as any).mixpanel.track(event_name, properties);
