c886983f84324f2a904cdef86f86b02b384e9739
Amit kumar Singh <notgetin18@gmail.com>
2024-04-01 13:18:54 +0530

    delete image

--- src/components/privacyPolicy/privacyPolicy.tsx
+++ src/components/privacyPolicy/privacyPolicy.tsx
@@ -1,60 +1,10 @@
 'use client'
-// import Link from 'next/link'
-// import React, { useEffect, useState } from 'react'
-// import OtpModal from '../modals/otpModal';
-// import { useSelector } from 'react-redux';
-// import { RootState } from '@/redux/store';
-// import { getPrivacyPosts } from '../sanity/getPost';
-// import BlogDetailsById from '../sanity/showBlogsDetails';
-
-// interface Post {
-   
-//     body: any; 
-//   }
-
-// const PrivacyPolicy = () => {
-//     const otpModal = useSelector((state: RootState) => state.auth.otpModal);
-//     const [privacy, setPrivacy] = useState<Post|null>();
-
-
-//     useEffect(() => {
-//         getPrivacyPosts().then(setPrivacy);
-//     }, []);
-
-
-//     console.log("privacy+++++", privacy)
-//     return (
-//         <div>
-//             {otpModal && <OtpModal />}
-//             <div className="mx-auto px-4 sm:px-6 lg:px-16  py-16 pb-28 xl:pb-8 pt-32">
-//                 <div className="container">
-//                     <div className="row pt-5 pb-5">
-//                         <div className=" sm:flex justify-between items-center text-center sm:text-left">
-//                             <h1 className="text-2xl sm:text-7xl mb-4   extrabold text-white text-center">
-//                                 Privacy Policy
-//                             </h1>
-//                             <img src="/lottie/Privacy Policy.gif" className="" />
-//                         </div>
-//                         {/* // paste here */}
-//                         <BlogDetailsById portableTextContent={privacy?.body} />
-
-//                     </div>
-//                 </div>
-//             </div>
-//         </div>
-//     )
-// }
-
-// export default PrivacyPolicy
-
-import Link from 'next/link';
 import React, { useEffect, useState } from 'react';
 import OtpModal from '../modals/otpModal';
 import { useSelector } from 'react-redux';
 import { RootState } from '@/redux/store';
 import { getPrivacyPosts } from '../sanity/getPost';
 import BlogDetailsById from '../sanity/showBlogsDetails';
-
 interface Privacy {
   body: any; 
 }
@@ -66,8 +16,6 @@ const PrivacyPolicy = () => {
     useEffect(() => {
         getPrivacyPosts().then(setPrivacy);
     }, []);
-
-    console.log("privacy+++++", privacy);
 
     return (
         <div>
@@ -86,9 +34,6 @@ const PrivacyPolicy = () => {
                           <BlogDetailsById portableTextContent={privacy.body} />
                         ) }
                         </div>
-                       
-                       
-
                     </div>
                 </div>
             </div>
