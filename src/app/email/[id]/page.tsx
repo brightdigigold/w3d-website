b97de6f5db2f80a14becd4b27896d3adfe9afea7
Amit kumar Singh <notgetin18@gmail.com>
2024-04-18 16:14:05 +0530

    kyc auto update

--- src/app/email/[id]/page.tsx
+++ src/app/email/[id]/page.tsx
@@ -1,12 +1,11 @@
 "use client"
 
-import React,{useEffect,useState} from 'react';
-import style from './verify.module.css';
+import React, { useEffect, useState } from 'react';
+// import style from './verify.module.css';
 // import { AesDecrypt } from '@/components/middleware';
 import Swal from 'sweetalert2';
 import { useRouter, } from "next/router";
 import Link from 'next/link';
-import { log } from 'console';
 import { AesDecrypt } from '@/components/helperFunctions';
 // import { log } from '@/components/logger';
 
@@ -19,45 +18,44 @@ import { AesDecrypt } from '@/components/helperFunctions';
 // 	}
 // };
 
-const PageEmailVerify = ({id}) => {
-    const router = useRouter();
-    const [message,setMessage] = useState('');
-    log("id in page : ", id)
-    useEffect(()=>{
-        verifyEmail()
-    },[])
-    const verifyEmail = ()=> {
-        fetch(`${process.env.baseUrl}/public/verify/${id}`)
-        .then((response) => response.json())
-        .then(async (data) => {
-          log("data in verify email ; ",data);
-          const decryptedData = await AesDecrypt(data.payload);
-          const parsedEmailData = JSON.parse(decryptedData);
-          log("parsedEmailData: ",parsedEmailData);
-          if(parsedEmailData.status){
-            setMessage(`${parsedEmailData.message}`);
-            // Swal.fire(
-            //     'Success',
-            //     `${parsedEmailData.data} ${parsedEmailData.message}`,
-            //     'success'
-            //   )
-            //   router.push("")
-          }else{
-            setMessage(`${parsedEmailData.message}`);
-          }
-        })
-        .catch(async (error)=>{
-          const decryptedData = await AesDecrypt(error.response.data.payload);
-          const parsedEmailData = JSON.parse(decryptedData);
+const PageEmailVerify = ({ id }) => {
+  const router = useRouter();
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    verifyEmail()
+  }, [])
+
+  const verifyEmail = () => {
+    fetch(`${process.env.baseUrl}/public/verify/${id}`)
+      .then((response) => response.json())
+      .then(async (data) => {
+        const decryptedData = await AesDecrypt(data.payload);
+        const parsedEmailData = JSON.parse(decryptedData);
+        if (parsedEmailData.status) {
           setMessage(`${parsedEmailData.message}`);
-        })
-    }
+          // Swal.fire(
+          //     'Success',
+          //     `${parsedEmailData.data} ${parsedEmailData.message}`,
+          //     'success'
+          //   )
+          //   router.push("")
+        } else {
+          setMessage(`${parsedEmailData.message}`);
+        }
+      })
+      .catch(async (error) => {
+        const decryptedData = await AesDecrypt(error.response.data.payload);
+        const parsedEmailData = JSON.parse(decryptedData);
+        setMessage(`${parsedEmailData.message}`);
+      })
+  }
   return (
-    <div className={style.emailverification}>
+    <div className="">
       <div className='row'>
         <div className='col-md-12'>
           <h1>{message}</h1>
-          <div className={style.home}><Link href="/">HOME</Link></div>
+          <div className=""><Link href="/">HOME</Link></div>
         </div>
       </div>
     </div>
