a29329aaca65bf80c1fee1557100950ad4511313
Amit kumar Singh <notgetin18@gmail.com>
2024-06-03 17:23:46 +0530

    custom checkout page

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -10,15 +10,15 @@ const page = () => {
 
   return (
     <RequireAuth>
-      <div className="mt-20">
+      <div className="">
         <div className="min-h-screen bg-theme flex items-center justify-center">
           <NextImage
             className=" absolute -bottom-12 -left-20 opacity-30"
             src="/bdgwhite.png"
             alt="digital gold"
             // priority
-            width={400} 
-            height={300} 
+            width={400}
+            height={300}
           />
 
           <CustomCheckout data={data} />
