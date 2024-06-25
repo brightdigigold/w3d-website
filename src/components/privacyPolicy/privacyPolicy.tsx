3dbda7e1910a444d9205c17a62935b6bf2fbade1
Amit kumar Singh <notgetin18@gmail.com>
2024-06-25 14:05:32 +0530

    added h1 tags

--- src/components/privacyPolicy/privacyPolicy.tsx
+++ src/components/privacyPolicy/privacyPolicy.tsx
@@ -21,25 +21,16 @@ const PrivacyPolicy = () => {
     return (
         <div>
             {otpModal && <OtpModal />}
-            <div className="mx-auto px-4 sm:px-6 lg:px-16 py-16 pb-28 xl:pb-8 pt-32">
-                <div className="container">
-                    <div className="row pt-5 pb-5">
-                        <div className="sm:flex justify-between items-center text-center sm:text-left">
-                            <h1 className="text-2xl sm:text-7xl mb-4 extrabold text-white text-center">
-                                Privacy Policy
-                            </h1>
-                            <img src="https://brightdigigold.s3.ap-south-1.amazonaws.com/Privacy+Policy.gif" className="" />
-                        </div>
-                        <div className='text-gray-100 text-justify prose-strong:text-white prose-strong:text-xl prose-lg prose-w-full prose-headings:text-yellow-400 prose-headings:text-2xl prose-headings:text-left prose-a:text-blue-400'>
-                            {privacy && (
-                                //   <BlogDetailsById portableTextContent={privacy.body} />
-                                <PortableText content={privacy.body} />
-                            )}
-                        </div>
+                <div className='container'>
+                    <div className='text-gray-100 text-justify prose-strong:text-white prose-strong:text-xl prose-lg prose-w-full prose-headings:text-yellow-400 prose-headings:text-2xl prose-headings:text-left prose-a:text-blue-400'>
+                        {privacy && (
+                            //   <BlogDetailsById portableTextContent={privacy.body} />
+                            <PortableText content={privacy.body} />
+                        )}
                     </div>
                 </div>
             </div>
-        </div>
+        // </div>
     );
 }
 
