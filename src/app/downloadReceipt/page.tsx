3d7789ffc889af317f967379a5b1e7684f57a10a
Amit kumar Singh <notgetin18@gmail.com>
2024-07-08 16:05:31 +0530

    mandir

--- src/app/downloadReceipt/page.tsx
+++ src/app/downloadReceipt/page.tsx
@@ -12,13 +12,74 @@ import fileDownload from 'js-file-download';
 import mixpanel from 'mixpanel-browser';
 import OtpModal from '@/components/modals/otpModal';
 
+const useTransactionForm = () => {
+    const [loading, setLoading] = useState(false);
+    const [pdfBlob, setPdfBlob] = useState<Blob | null>(null);
+    const [errorMessage, setErrorMessage] = useState<string | null>(null);
+
+    const handleResponse = async (response: any) => {
+        const decryptedData = await funcForDecrypt(response.data.payload);
+        const dataOfParticularTransactionUTR = JSON.parse(decryptedData);
+
+        if (dataOfParticularTransactionUTR.statusCode === 200) {
+            // Convert Buffer to Blob
+            const buffer = Buffer.from(dataOfParticularTransactionUTR.data.data);
+            const blob = new Blob([buffer], { type: 'application/pdf' });
+            setPdfBlob(blob);
+        }
+    };
+
+    const handleError = async (error: any) => {
+        console.error("Error response:", error.response);
+        if (error.response && error.response.data && error.response.data.payload) {
+            try {
+                let decryptedError = await funcForDecrypt(error.response.data.payload);
+                console.error("Decrypted error message:", decryptedError);
+                setErrorMessage(JSON.parse(decryptedError).message);
+            } catch (decryptionError) {
+                console.error("Error decrypting error message:", decryptionError);
+                setErrorMessage("An error occurred while processing your request.");
+            }
+        } else {
+            console.error("Unknown error:", error);
+            setErrorMessage("An unknown error occurred.");
+        }
+    };
+
+    const handleSubmit = async (values: any) => {
+        setLoading(true);
+        setErrorMessage(null);
+        try {
+            const token = localStorage.getItem("token");
+            const configHeaders = {
+                headers: {
+                    authorization: `Bearer ${token}`,
+                    "Content-Type": "application/json",
+                },
+            };
+            const data = { utr: values.utr };
+            const resAfterEncrypt = await AesEncrypt(data);
+            const response = await axios.post(
+                `${process.env.baseUrl}/user/receipt`,
+                { payload: resAfterEncrypt },
+                configHeaders
+            );
+            await handleResponse(response);
+        } catch (error: any) {
+            await handleError(error);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    return { loading, pdfBlob, errorMessage, handleSubmit };
+};
+
 const Page = () => {
     const otpModal = useSelector((state: RootState) => state.auth.otpModal);
     const showProfileForm = useSelector((state: RootState) => state.auth.showProfileForm);
     const dispatch = useDispatch();
-    const [loading, setLoading] = useState(false);
-    const [pdfBlob, setPdfBlob] = useState<Blob | null>(null);
-    const [errorMessage, setErrorMessage] = useState<string | null>(null);
+    const { loading, pdfBlob, errorMessage, handleSubmit } = useTransactionForm();
 
     const onClose = () => {
         dispatch(setShowProfileForm(false));
@@ -26,64 +87,16 @@ const Page = () => {
 
     const validationSchema = Yup.object().shape({
         utr: Yup.string()
-            .required('UTR is required').length(12, 'UTR should be of 12 digits'),
+            .required('UTR is required')
+            .length(12, 'UTR should be of 12 digits'),
     });
 
     const formik = useFormik({
         initialValues: {
             utr: "453525325220",
         },
-        validationSchema: validationSchema,
-        onSubmit: async (values) => {
-            setLoading(true);
-            setErrorMessage(null); // Reset error message on new submit
-            try {
-                const token = localStorage.getItem("token");
-                const configHeaders = {
-                    headers: {
-                        authorization: `Bearer ${token}`,
-                        "Content-Type": "application/json",
-                    },
-                };
-                const data = { utr: values.utr };
-                const resAfterEncrypt = await AesEncrypt(data);
-
-                const response = await axios.post(
-                    `${process.env.baseUrl}/user/receipt`,
-                    { payload: resAfterEncrypt },
-                    configHeaders
-                );
-
-                const decryptedData = await funcForDecrypt(response.data.payload);
-                const dataOfParticularTransactionUTR = JSON.parse(decryptedData);
-
-                if (dataOfParticularTransactionUTR.statusCode === 200) {
-                    // Convert Buffer to Blob
-                    const buffer = Buffer.from(dataOfParticularTransactionUTR.data.data);
-                    const blob = new Blob([buffer], { type: 'application/pdf' });
-                    setPdfBlob(blob);
-                } else if (dataOfParticularTransactionUTR.statusCode === 406) {
-                    setErrorMessage(dataOfParticularTransactionUTR.message);
-                }
-            } catch (error: any) {
-                console.error("Error response:", error.response);
-                if (error.response && error.response.data && error.response.data.payload) {
-                    try {
-                        let error1 = await funcForDecrypt(error.response.data.payload);
-                        console.error("Decrypted error message:", error1);
-                        setErrorMessage(JSON.parse(error1).message);
-                    } catch (decryptionError) {
-                        console.error("Error decrypting error message:", decryptionError);
-                        setErrorMessage("An error occurred while processing your request.");
-                    }
-                } else {
-                    console.error("Unknown error:", error);
-                    setErrorMessage("An unknown error occurred.");
-                }
-            } finally {
-                setLoading(false);
-            }
-        },
+        validationSchema,
+        onSubmit: handleSubmit,
     });
 
     const handleDownload = () => {
@@ -96,12 +109,11 @@ const Page = () => {
     return (
         <div className='mt-20 w-full p-4'>
             {otpModal && <OtpModal />}
-
             {showProfileForm && (
                 <SetProfileForNewUser isOpen={showProfileForm} onClose={onClose} />
             )}
-            <div className="flex  items-center justify-center h-[650px]">
-                <div className="p-4 border border-yellow-200 shadow-[0_35px_60px_-15px_rgba(0,0,0,0.5)] rounded-md h-1/3 sm:h-1/3 w-screen sm:w-1/3 ">
+            <div className="flex items-center justify-center h-[650px]">
+                <div className="p-4 border border-yellow-200 shadow-[0_35px_60px_-15px_rgba(0,0,0,0.5)] rounded-md h-1/3 sm:h-1/3 w-screen sm:w-1/3">
                     <form onSubmit={formik.handleSubmit}>
                         <FormInput
                             type="text"
@@ -130,7 +142,7 @@ const Page = () => {
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default Page;
