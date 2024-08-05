a21eff8112baab507c0bbaea396b5bc4f36bf16c
Amit kumar Singh <notgetin18@gmail.com>
2024-08-05 16:18:37 +0530

    banner

--- src/app/coins/page.tsx
+++ src/app/coins/page.tsx
@@ -1,66 +1,55 @@
-import Coins from '@/components/coins/coinsPage'
-import React from 'react'
+import Coins from '@/components/coins/coinsPage';
+import React from 'react';
 import { Metadata } from "next";
 import Image from "next/image";
 
-const page = () => {
+const Page = () => {
   return (
-    <div className='pb-28 xl:pb-8 pt-16'>
-
-      {/* <div className='flex bg-[#C8E9F2] justify-between'>
+    <div className='pb-28 xl:pb-8 '>
+      <div className='flex bg-[#C8E9F2] justify-between mx-auto container'>
         <Image
           src="/gaytri.png"
           alt="gold and silver coin banner"
-          className="rounded-b "
-          width={476}
-          height={1416}
+          className="rounded-b md:mt-4"
+          width={376}
+          height={416}
           priority={true}
           layout='intrinsic'
         />
-        <div className='h-screen grid place-items-center'>
-          <div className='mt-44'>
-            <p className='text-black font-extrabold bg-yellow-300 sm:text-3xl xl:text-5xl'>
+        <div className='grid place-items-center'>
+          <div className='2xl:mt-44'>
+            <div className="text-black text-center text-4xl lg:text-5xl">
               GET FREE DELIVERY
-            </p>
-            <p className='text-black font-extrabold text-center text-4xl pt-2'>
+            </div>
+            <p className='text-black text-center text-5xl pt-2 lg:text-3xl'>
               of Gold and Silver Coins
             </p>
-            <p className='text-black font-extrabold text-center text-3xl pt-2'>
+            <p className='text-black text-center text-4xl pt-2 lg:text-2xl'>
               at your Doorstep
             </p>
           </div>
         </div>
-
         <Image
           src="/Coins stand.png"
           alt="gold and silver coin banner"
-          className="rounded-b"
-          width={662}
-          height={0}
-          priority={true}
-          layout='intrinsic'
-        />
-      </div> */}
-
-      <div className="flex justify-center items-center">
-        <Image
-          src="https://brightdigigold.s3.ap-south-1.amazonaws.com/banner/ProductBannerNEW.jpg"
-          alt="gold and silver coin banner"
-          className="rounded-b"
-          width={1600}
-          height={700}
+          className="rounded-b mt-36"
+          width={462}
+          height={662}
           priority={true}
           layout='intrinsic'
         />
       </div>
-      <h1 className="text-white text-2xl sm:text-4xl extrabold text-center mt-8">Our Coins</h1>
+      <h1 className="text-white text-2xl sm:text-4xl pb-8 extrabold text-center mt-8">
+        Our Coins
+      </h1>
       <Coins />
     </div>
   );
-}
+};
 
-export default page
+export default Page;
+
 export const metadata: Metadata = {
-  title: "Gold Coins and Silver Coins- Bright DiGi Gold  ",
+  title: "Gold Coins and Silver Coins - Bright DiGi Gold",
   description: "Explore our wide range of Gold and Silver coins with 24k purity and 100% safe and secure delivery.",
 };
