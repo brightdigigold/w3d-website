6e1c6cb2645bf33d78c2e59e890d227c997c1c8b
Amit kumar Singh <notgetin18@gmail.com>
2024-08-05 17:17:41 +0530

    banner

--- src/app/coins/page.tsx
+++ src/app/coins/page.tsx
@@ -6,37 +6,33 @@ import Image from "next/image";
 const Page = () => {
   return (
     <div className='pb-28 xl:pb-8 '>
-      <div className='flex bg-[#C8E9F2] justify-between mx-auto container'>
+      <div className='flex bg-[#C8E9F2] justify-between mx-auto max-w-[1600px]'>
         <Image
           src="/gaytri.png"
-          alt="gold and silver coin banner"
+          alt="gold and silver coin banner "
           className="rounded-b md:mt-4"
-          width={376}
-          height={416}
+          width={476}
+          height={0}
           priority={true}
           layout='intrinsic'
         />
         <div className='grid place-items-center'>
           <div className='2xl:mt-44'>
-            <div className="text-black text-center text-4xl lg:text-5xl">
+            <div className="text-black text-center pt-1 poppins-bold text-5xl">
               GET FREE DELIVERY
             </div>
-            <p className='text-black text-center text-5xl pt-2 lg:text-3xl'>
+            <p className='text-black text-center pt-1 poppins-semibold text-4xl'>
               of Gold and Silver Coins
             </p>
-            <p className='text-black text-center text-4xl pt-2 lg:text-2xl'>
+            <p className='text-black text-center pt-1 text-3xl'>
               at your Doorstep
             </p>
           </div>
         </div>
-        <Image
-          src="/Coins stand.png"
+        <img
+          src="/Coins stand (1).png"
           alt="gold and silver coin banner"
-          className="rounded-b mt-36"
-          width={462}
-          height={662}
-          priority={true}
-          layout='intrinsic'
+          className="rounded-b mt-64 h-2/6 w-2/6 mr-5"
         />
       </div>
       <h1 className="text-white text-2xl sm:text-4xl pb-8 extrabold text-center mt-8">
