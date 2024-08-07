a6ca9f569957a3fcb937904c3a1558ca0404aef6
Amit kumar Singh <notgetin18@gmail.com>
2024-08-07 13:43:29 +0530

    navbar done

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -99,11 +99,11 @@ const Navbar = () => {
 
           <div className="mx-auto px-2 sm:px-6 xl:px-16 py-2 z-10">
             <div className="relative flex h-16 items-center justify-between">
-              <div className="absolute inset-y-0 left-0 flex items-center xl:hidden">
+              <div className="absolute inset-y-0 left-0 flex items-center xl:hidden justify-between">
                 {/* Mobile menu button*/}
                 <Disclosure.Button
+                  className="relative inline-flex items-center justify-center rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
                   onClick={handleSidebarClick}
-                  className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
                 >
                   <span className="absolute -inset-0.5" />
                   <span className="sr-only">Open main menu</span>
@@ -112,10 +112,25 @@ const Navbar = () => {
                     className="block h-8 w-8 text-yellow-400"
                     aria-hidden="true"
                   />
+
                 </Disclosure.Button>
+                <Link href="/" prefetch={true}>
+                  {/* <a className="flex items-center justify-center"> */}
+                  <Image
+                    src="https://brightdigigold.s3.ap-south-1.amazonaws.com/bdgLogo.png"
+                    alt="Bright Digi Gold Logo"
+                    className="h-18 w-18"
+                    width={80}
+                    height={64}
+                    priority={true}
+                    layout="intrinsic"
+                    sizes="(max-width: 64px) 100vw, 64px"
+                  />
+                  {/* </a> */}
+                </Link>
               </div>
               <div className="flex flex-1 items-center justify-center xl:items-stretch xl:justify-start">
-                <div className="flex flex-shrink-0 items-center">
+                <div className="flex-shrink-0 items-center hidden sm:block">
                   <Link href="/" legacyBehavior prefetch={true}>
                     <a className="flex items-center justify-center">
                       <Image
@@ -147,7 +162,7 @@ const Navbar = () => {
                   About
                 </Link>
               </div>
-              
+
               {isloggedIn && (
                 <>
                   <div className={clsx({ 'border-b-4 border-yellow-500 rounded-md': pathname == '/dashboard', })}>
