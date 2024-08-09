41584e29aae3cb55dc24b061a3140807e41fe503
Amit kumar Singh <notgetin18@gmail.com>
2024-08-09 17:44:27 +0530

    site map updated

--- src/components/requireAuth.tsx
+++ src/components/requireAuth.tsx
@@ -11,37 +11,30 @@ const RequireAuth = ({ children }) => {
     const purpose = useSelector((state: RootState) => state.auth.purpoes);
     const path = window.location.pathname
 
-    console.log("pathname", window.location.pathname)
-
 
     useEffect(() => {
 
         if (isloggedIn) {
-            // console.log('You are from 20');
             router.replace(window.location.pathname);
             return;
         }
 
         if (isLoggedInForTempleReceipt && purpose === 'receipt' && path == '/downloadReceipt') {
-            // console.log('You are from 26');
             router.replace('/downloadReceipt');
             return;
         }
 
         if (isLoggedInForTempleReceipt && purpose === 'receipt') {
-            // console.log('You are from 31');
             router.replace('/downloadReceipt');
             return;
         }
 
         if (!isloggedIn && !isLoggedInForTempleReceipt) {
-            // console.log('You are from 38');
             router.replace('/');
             return;
         }
 
         if (!isloggedIn) {
-            // console.log('You are from 44');
             router.replace('/');
             return;
         }
