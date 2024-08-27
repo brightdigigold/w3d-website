0d7d5bf939ef5427c58f65b8c8c1b7dc55161393
Amit kumar Singh <notgetin18@gmail.com>
2024-08-27 13:36:57 +0530

    blog

--- src/components/homepage/blog.tsx
+++ src/components/homepage/blog.tsx
@@ -34,78 +34,80 @@ export default function Blog() {
 
   return <>
     <div className="bg-theme" >
-      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 py-8">
+      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 py-12">
         <Link href="/digital-gold-blog" className="cursor-pointer">
           <h1 className="text-center text-yellow-500 text-3xl sm:text-5xl extrabold mb-0 sm:mb-6">
             Our Blogs
           </h1>
         </Link>
-        <Swiper
-          loop={true}
-          breakpoints={{
-            320: {
-              slidesPerView: 2,
-              spaceBetween: 5,
-            },
-            640: {
-              slidesPerView: 2,
-              spaceBetween: 5,
-            },
-            768: {
-              slidesPerView: 2,
-              spaceBetween: 5,
-            },
-            1024: {
-              slidesPerView: 3,
-              spaceBetween: 5,
-            },
-          }}
-          autoplay={{
-            delay: 2500,
-            disableOnInteraction: false,
-          }}
-          effect={"coverflow"}
-          // grabCursor={true}
-          centeredSlides={true}
-          coverflowEffect={{
-            rotate: 50,
-            stretch: 0,
-            depth: 100,
-            modifier: 1,
-            slideShadows: true,
-          }}
-          navigation={true}
-          modules={[EffectCoverflow, Navigation, Autoplay]}
-          className="mySwiperblog"
-          style={{ padding: "0 20px !important" }}
-        >
-          {posts.map((feature, index) => (
-            <SwiperSlide
-              key={`${index}-Slider`}
-              className="relative swiper-slide p-0 sm:p-4 pt-10"
-            >
-              <div className="border border-1 rounded-b-2xl rounded-t-xl">
-                <Link href={`/digital-gold-blog/${feature.slug.current}`} passHref>
-                  <div className="rounded-2xl h-56 sm:h-72 relative">
-                    <div className="flex justify-center">
-                      <Image
-                        src={feature.thumbImage?.asset?.url}
-                        className="rounded-t-xl cursor-pointer"
-                        alt="insite"
-                        width={1920}
-                        height={800}
-                        style={{
-                          maxWidth: "100%",
-                          height: "auto"
-                        }} />
+        <div className="pb:10">
+          <Swiper
+            loop={true}
+            breakpoints={{
+              320: {
+                slidesPerView: 2,
+                spaceBetween: 5,
+              },
+              640: {
+                slidesPerView: 2,
+                spaceBetween: 5,
+              },
+              768: {
+                slidesPerView: 2,
+                spaceBetween: 5,
+              },
+              1024: {
+                slidesPerView: 3,
+                spaceBetween: 5,
+              },
+            }}
+            autoplay={{
+              delay: 2500,
+              disableOnInteraction: false,
+            }}
+            effect={"coverflow"}
+            // grabCursor={true}
+            centeredSlides={true}
+            coverflowEffect={{
+              rotate: 50,
+              stretch: 0,
+              depth: 100,
+              modifier: 1,
+              slideShadows: true,
+            }}
+            navigation={true}
+            modules={[EffectCoverflow, Navigation, Autoplay]}
+            className="mySwiperblog"
+            style={{ padding: "0 20px !important" }}
+          >
+            {posts.map((feature, index) => (
+              <SwiperSlide
+                key={`${index}-Slider`}
+                className="relative swiper-slide p-0 sm:p-4 pt-4"
+              >
+                <div className="border border-1 rounded-b-lg rounded-t-xl pb-1">
+                  <Link href={`/digital-gold-blog/${feature.slug.current}`} passHref>
+                    <div className="rounded-lg h-32 sm:h-72 relative">
+                      <div className="flex justify-center">
+                        <Image
+                          src={feature.thumbImage?.asset?.url}
+                          className="rounded-t-xl cursor-pointer"
+                          alt="insite"
+                          width={1920}
+                          height={800}
+                          style={{
+                            maxWidth: "100%",
+                            height: "auto"
+                          }} />
+                      </div>
+                      <p className="text-white text-center text-lg poppins-regular line-clamp-1">{feature?.title}</p>
                     </div>
-                    <p className="text-white text-center text-lg">{feature?.title}</p>
-                  </div>
-                </Link>
-              </div>
-            </SwiperSlide>
-          ))}
-        </Swiper>
+                  </Link>
+                </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
       </div>
     </div>
   </>;
