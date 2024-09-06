6d98fc70868d665673d7a83e40166b0b1c4832ea
monk-27 <exquisiteshashi@gmail.com>
2024-09-06 13:50:01 +0530

    custom button

--- src/components/customButton.tsx
+++ src/components/customButton.tsx
@@ -1,4 +1,5 @@
 import { customButtonProps } from "@/types";
+import clsx from "clsx";
 import Image from "next/image";
 import { ImSpinner8 } from "react-icons/im";
 
@@ -11,24 +12,31 @@ const CustomButton = ({
   Icon,
   isDisabled,
   loading,
-  noPadding = false, 
+  noPadding = false,
 }: customButtonProps) => {
   return (
     <button
       disabled={isDisabled || loading} // Disable the button if loading
       type={btnType || "button"}
-      className={`flex justify-center items-center ${containerStyles}`} // Flexbox centering both horizontally and vertically
+      // className={`flex justify-center items-center ${containerStyles}`} // Flexbox centering both horizontally and vertically
+      className={clsx(
+        {
+          "flex justify-center items-center": Icon || loading, 
+          "custom-btn": !(Icon || loading), 
+        },
+        containerStyles 
+      )}
       onClick={handleClick}
       style={{
         minWidth: '120px',
         padding: noPadding ? '0px' : '8px',
       }}
     >
-        {loading ? (
-          <ImSpinner8 className="animate-spin m-1" /> // Spinner is centered vertically and horizontally
-        ) : (
-          <span className={`text-center ${textStyles}`}>{title}</span> // Centered text when not loading
-        )}
+      {loading ? (
+        <ImSpinner8 className="animate-spin m-1" /> // Spinner is centered vertically and horizontally
+      ) : (
+        <span className={`text-center ${textStyles}`}>{title}</span> // Centered text when not loading
+      )}
 
       {Icon && !loading && (
         <div className="relative w-6 h-6 ml-2">
