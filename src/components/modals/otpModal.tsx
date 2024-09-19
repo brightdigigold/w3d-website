edb8e718928a7363c55f039ecb3b680cd5115e08
Amit kumar Singh <notgetin18@gmail.com>
2024-09-19 14:17:30 +0530

    corporate login

--- src/components/modals/otpModal.tsx
+++ src/components/modals/otpModal.tsx
@@ -275,7 +275,7 @@ export default function OtpModal() {
               leaveFrom="opacity-100 translate-y-0 sm:scale-100"
               leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             >
-              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-theme text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-theme text-left shadow-xl transition-all sm:my-6 sm:w-full sm:max-w-lg">
                 <div className="px-5 pt-5 sm:p-6 sm:pb-3">
                   <div>
                     <img src="/LogoOtp.png" className="h-16 mb-4 mx-auto" />
