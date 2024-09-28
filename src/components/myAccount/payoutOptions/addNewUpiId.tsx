8702a209d287f6391d0e707f5fe7762015e7349d
Amit kumar Singh <notgetin18@gmail.com>
2024-09-28 16:16:16 +0530

    pan details done

--- src/components/myAccount/payoutOptions/addNewUpiId.tsx
+++ src/components/myAccount/payoutOptions/addNewUpiId.tsx
@@ -1,4 +1,4 @@
-import { Fragment, useEffect, useRef, useState } from "react";
+import { Fragment, useRef, useState } from "react";
 import { Dialog, Transition } from "@headlessui/react";
 import { funForAesEncrypt, funcForDecrypt } from "@/components/helperFunctions";
 import axios from "axios";
